8ef9c49de9bd2b300487334ee1249e0308cbe00b
artemef1996@gmail.com <artemef1996@gmail.com>
2024-06-22 17:59:39 +0300

    task4

--- opExamen/Program.cs
+++ opExamen/Program.cs
@@ -4,35 +4,81 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Введите размер массива: ");
+			Console.Write("Введите размер массивов: ");
 			int arraySize = int.Parse(Console.ReadLine());
 
-			MyClass[] myArray = new MyClass[arraySize];
+			MySecondClass[] array1 = new MySecondClass[arraySize];
+			MySecondClass[] array2 = new MySecondClass[arraySize];
 
 			Random random = new Random();
 			for (int i = 0; i < arraySize; i++)
 			{
-				int randomInt = random.Next(1, 101);
-				string randomString = "String" + randomInt;
-				myArray[i] = new MyClass(randomInt, randomString);
+				array1[i] = new MySecondClass("String" + i.ToString(), random.Next(0, 2) == 0);
+				array2[i] = new MySecondClass("String" + i.ToString(), random.Next(0, 2) == 0);
 			}
 
-			for (int i = 0; i < arraySize; i++)
+			int falseCountArray1 = CountFalseValues(array1);
+			int falseCountArray2 = CountFalseValues(array2);
+
+			Console.WriteLine("Первый массив массив:");
+			foreach (var item in array1)
 			{
-				Console.WriteLine($"Элемент {i}: Число = {myArray[i].Number}, Строка = {myArray[i].Text}");
+				Console.Write($"{item.BooleanValue}, ");
 			}
+			Console.WriteLine($"\nКоличество значений false в первом массиве: {falseCountArray1}");
+
+			Console.WriteLine("Второй массив массив:");
+			foreach (var item in array2)
+			{
+				Console.Write($"{item.BooleanValue}, ");
+			}
+			Console.WriteLine($"\nКоличество значений false во втором массиве: {falseCountArray2}");
+
+			if (falseCountArray1 > falseCountArray2)
+			{
+				Console.WriteLine("Значение false чаще встречается в первом массиве.");
+			}
+			else if (falseCountArray1 < falseCountArray2)
+			{
+				Console.WriteLine("Значение false чаще встречается во втором массиве.");
+			}
+			else
+			{
+				Console.WriteLine("Значение false встречается одинаково часто в обоих массивах.");
+			}
+		}
+
+		static int CountFalseValues(MySecondClass[] array)
+		{
+			int count = 0;
+			foreach (var item in array)
+			{
+				if (!item.BooleanValue)
+				{
+					count++;
+				}
+			}
+			return count;
 		}
 	}
 
 	class MyClass
 	{
-		public int Number { get; private set; }
-		public string Text { get; private set; }
+		public string StringValue { get; private set; }
+		public bool BooleanValue { get; private set; }
 
-		public MyClass(int number, string text)
+		public MyClass(string stringValue, bool booleanValue)
 		{
-			Number = number;
-			Text = text;
+			StringValue = stringValue;
+			BooleanValue = booleanValue;
+		}
+	}
+
+	class MySecondClass : MyClass
+	{
+		public MySecondClass(string stringValue, bool booleanValue)
+			: base(stringValue, booleanValue)
+		{
 		}
 	}
 }
