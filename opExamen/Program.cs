592d149952d43868d836d37dff34db009673d0ab
artemef1996@gmail.com <artemef1996@gmail.com>
2024-06-22 17:39:16 +0300

    task2

--- opExamen/Program.cs
+++ opExamen/Program.cs
@@ -6,14 +6,11 @@ namespace opExamen
 	{
 		static void Main(string[] args)
 		{
-			// Создание экземпляра класса MyClass
 			MyClass myObject = new MyClass();
 
-			// Вызов приватного метода с использованием рефлексии
 			MethodInfo privateMethod = typeof(MyClass).GetMethod("PrivateMethod", BindingFlags.NonPublic | BindingFlags.Instance);
 			if (privateMethod != null)
 			{
-				// Передача сообщения в приватный метод
 				privateMethod.Invoke(myObject, new object[] { "Hello from the private method!" });
 			}
 			else
@@ -24,7 +21,6 @@ namespace opExamen
 	}
 	class MyClass
 	{
-		// Приватный метод, выводящий сообщение на консоль
 		private void PrivateMethod(string message)
 		{
 			Console.WriteLine(message);
