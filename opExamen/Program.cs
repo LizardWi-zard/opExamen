78ae2b70d8818a9873d85037748185d9a78baea2
artemef1996@gmail.com <artemef1996@gmail.com>
2024-06-22 17:38:45 +0300

    task1

--- opExamen/Program.cs
+++ opExamen/Program.cs
@@ -1,22 +1,28 @@
-﻿namespace opExamen
+﻿using System.Reflection;
+
+namespace opExamen
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			// Ввод размерности массива с клавиатуры
 			Console.Write("Введите размер массива: ");
 			int arraySize = int.Parse(Console.ReadLine());
 
+			// Создание массива элементов класса MyClass
 			MyClass[] myArray = new MyClass[arraySize];
 
+			// Заполнение массива произвольными значениями
 			Random random = new Random();
 			for (int i = 0; i < arraySize; i++)
 			{
-				int randomInt = random.Next(1, 101);
-				string randomString = "String" + randomInt;
+				int randomInt = random.Next(1, 101); // случайное целое число от 1 до 100
+				string randomString = "String" + randomInt; // случайная строка
 				myArray[i] = new MyClass(randomInt, randomString);
 			}
 
+			// Вывод значений элементов массива на консоль
 			for (int i = 0; i < arraySize; i++)
 			{
 				Console.WriteLine($"Элемент {i}: Число = {myArray[i].Number}, Строка = {myArray[i].Text}");
@@ -26,9 +32,11 @@
 
 	class MyClass
 	{
+		// Автоматически реализуемые свойства
 		public int Number { get; private set; }
 		public string Text { get; private set; }
 
+		// Конструктор, принимающий 2 параметра (целочисленный и строковый)
 		public MyClass(int number, string text)
 		{
 			Number = number;
