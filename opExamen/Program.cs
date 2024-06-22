63eab91f5a9ee0dd849393dec9a000bb3785fe1e
artemef1996@gmail.com <artemef1996@gmail.com>
2024-06-22 18:09:53 +0300

    task7

--- opExamen/Program.cs
+++ opExamen/Program.cs
@@ -4,35 +4,21 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Введите размер массива: ");
-			int arraySize = int.Parse(Console.ReadLine());
+			MyClass<int> intInstance = new MyClass<int>();
+			MyClass<string> stringInstance = new MyClass<string>();
+			MyClass<double> doubleInstance = new MyClass<double>();
 
-			MyClass[] myArray = new MyClass[arraySize];
-
-			Random random = new Random();
-			for (int i = 0; i < arraySize; i++)
-			{
-				int randomInt = random.Next(1, 101);
-				string randomString = "String" + randomInt;
-				myArray[i] = new MyClass(randomInt, randomString);
-			}
-
-			for (int i = 0; i < arraySize; i++)
-			{
-				Console.WriteLine($"Элемент {i}: Число = {myArray[i].Number}, Строка = {myArray[i].Text}");
-			}
+			intInstance.PrintType();
+			stringInstance.PrintType();
+			doubleInstance.PrintType();
 		}
 	}
 
-	class MyClass
+	class MyClass<T>
 	{
-		public int Number { get; private set; }
-		public string Text { get; private set; }
-
-		public MyClass(int number, string text)
+		public void PrintType()
 		{
-			Number = number;
-			Text = text;
+			Console.WriteLine($"Тип данных: {typeof(T)}");
 		}
 	}
 }
