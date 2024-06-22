428c5c8c632c62d005adab8fa08b3e515ea2a1e5
artemef1996@gmail.com <artemef1996@gmail.com>
2024-06-22 17:49:25 +0300

    task3

--- opExamen/Program.cs
+++ opExamen/Program.cs
@@ -7,32 +7,40 @@
 			Console.Write("Введите размер массива: ");
 			int arraySize = int.Parse(Console.ReadLine());
 
-			MyClass[] myArray = new MyClass[arraySize];
-
+			int[] array = new int[arraySize];
 			Random random = new Random();
-			for (int i = 0; i < arraySize; i++)
-			{
-				int randomInt = random.Next(1, 101);
-				string randomString = "String" + randomInt;
-				myArray[i] = new MyClass(randomInt, randomString);
-			}
 
 			for (int i = 0; i < arraySize; i++)
 			{
-				Console.WriteLine($"Элемент {i}: Число = {myArray[i].Number}, Строка = {myArray[i].Text}");
+				array[i] = random.Next(0, arraySize + 1);
 			}
+
+			Console.WriteLine("Сгенерированный массив:");
+			Console.WriteLine(string.Join(", ", array));
+
+			double median = CalculateMedian(array);
+
+			Console.WriteLine($"Медианное значение массива: {median}");
 		}
-	}
 
-	class MyClass
-	{
-		public int Number { get; private set; }
-		public string Text { get; private set; }
-
-		public MyClass(int number, string text)
+		static double CalculateMedian(int[] array)
 		{
-			Number = number;
-			Text = text;
+			int[] sortedArray = (int[])array.Clone();
+			Array.Sort(sortedArray);
+
+			Console.WriteLine("Отсортированный массив:");
+			Console.WriteLine(string.Join(", ", sortedArray));
+
+			int middleIndex = sortedArray.Length / 2;
+
+			if (sortedArray.Length % 2 == 0)
+			{
+				return (sortedArray[middleIndex - 1] + sortedArray[middleIndex]) / 2.0;
+			}
+			else
+			{
+				return sortedArray[middleIndex];
+			}
 		}
 	}
 }
